faf440cfcda675c32dd62289ff56d34dadfcb4e0
Filipa Senra <39194324+filipasenra@users.noreply.github.com>
2021-10-31 14:12:27 +0000

    fix(comment-methods): comment methods so the 1st exercise has 100% coverage

--- printing_service/src/main/java/printing_service/PrintManager.java
+++ printing_service/src/main/java/printing_service/PrintManager.java
@@ -39,6 +39,7 @@ public class PrintManager {
      *
      * @return The percentage of documents that will be printed.
      */
+    /*
     int satisfiedPrintingPercentage(List<Document> documents, List<Printer> printers) {
 
         int numPagesToPrint = documents.stream().mapToInt(doc -> doc.num_pages).sum();
@@ -46,6 +47,7 @@ public class PrintManager {
 
         return capacity * 100 / numPagesToPrint;
     }
+     */
 
     // TODO: Make this method empty for workshop
     /**
@@ -54,6 +56,7 @@ public class PrintManager {
      *
      * @return A list containing the ids of the documents that will not be printed by the printing service.
      */
+    /*
     List<Integer> missingDocuments(List<Document> documents, List<Printer> printers) {
 
         List<Integer> result = new ArrayList<>();
@@ -69,12 +72,14 @@ public class PrintManager {
 
         return result;
     }
+     */
 
     /**
      * Obtain a list with the ids of the printers that will not be used to satisfy the printing service.
      *
      * @return A list containing the ids of the printers that will not be used by the printing service.
      */
+    /*
     List<Printer> unusedPrinters(List<Document> documents, List<Printer> printers) {
 
         List<Printer> result = new ArrayList<>();
@@ -90,5 +95,6 @@ public class PrintManager {
 
         return result;
     }
+     */
 
 }
